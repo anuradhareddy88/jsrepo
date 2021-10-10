6f4081429dd0fb162b4a5c03d53a6b33d524ce27
anuradhareddy88 <92051959+anuradhareddy88@users.noreply.github.com>
2021-10-11 01:24:33 +0530

    first

---
+++ object_destructuring.js
@@ -0,0 +1,29 @@
+let a, b, rest;
+[a, b] = [10, 20];
+console.log(a); // 10
+console.log(b); // 20
+
+[a, b, ...rest] = [10, 20, 30, 40, 50];
+console.log(a); // 10
+console.log(b); // 20
+console.log(rest); // [30, 40, 50]
+
+// in object
+({ a, b } = { a: 10, b: 20 });
+console.log(a); // 10
+console.log(b); // 20
+
+// Stage 4(finished) proposal
+({a, b, ...rest} = {a: 10, b: 20, c: 30, d: 40});
+console.log(a); // 10
+console.log(b); // 20
+console.log(rest); // {c: 30, d: 40}
+
+// in array
+const colors = ['one', 'two'];
+
+const [red, yellow, green, blue] = colors;
+console.log(red); // "one"
+console.log(yellow); // "two"
+console.log(green); // undefined
+console.log(blue);  //undefined
