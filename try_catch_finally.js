b2eea31ecb779a3a200267d6cb9d0d0eb4a9ec5f
anuradhareddy88 <92051959+anuradhareddy88@users.noreply.github.com>
2021-10-13 00:30:01 +0530

    trycatch

---
+++ try_catch_finally.js
@@ -0,0 +1,43 @@
+const name = 'eshu';
+try{
+    wrongname("it showing error");
+}
+catch(truename){
+    console.log("it showing  name");
+}  
+finally{
+    console.log("name is eshu");
+}
+
+//try,catch,finally
+const pets=['dog','cat','bird']
+try{
+    petcolor("i love cat white color");
+}
+catch(petname){
+    console.log("dog name is dogo");
+}
+finally{
+    console.log("pet name is dogo color is black");
+}
+
+//try,catch,finally
+
+const array =[2,4,6,8]
+const array2=[3,5,7,9];
+const a=2;
+const b = 4;
+try {
+    console.log( `all are even numbers ${array1}`);
+    
+    //return a*b;
+}
+catch(array2){
+    console.log(`all are odd numbers ${array2}`)
+    //total= a+b;
+    //return total;
+}
+finally{
+    console.log(`total numbers are ${array},${array2}`);
+}
+
